103bd6d07207b29caff8951cd891a31a2d39e6f9
Hicret <hiicretay@hotmail.com>
2020-04-21 20:50:04 +0300

    Initial commit

---
+++ Girilen_Sayiya_Kadar_Toplam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ardisik_sayi_bastiran_kod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            int sayi;
+            Console.WriteLine("kac sayi girilsin: ");
+            sayi = int.Parse(Console.ReadLine());
+
+            for ( int i=0;i<=sayi;i++)
+            {
+                toplam = toplam + i;   //girilen sayıya kadar olan sayıların toplamı
+            }
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+        }
+    }
+}
