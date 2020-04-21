5261c56adde2a3a32c80a37de685ff0d58ddbc57
Hicret <hiicretay@hotmail.com>
2020-04-21 22:17:26 +0300

    21.04.2020

---
+++ Aylik_Mesai_Ucreti.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aylik_mesai_ucreti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("saatlik ucreti giriniz: ");
+            double ucret = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("calisma saatini giriniz: ");
+            int saat = Convert.ToInt32(Console.ReadLine());
+
+            double maas = ucret * saat;
+            Console.WriteLine("aylik kazanciniz: " + maas);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dairenin_Alanini_Hesaplama.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dairenin_alanini_hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double alan, yaricap;
+            Console.WriteLine("yaricapi giriniz: ");
+            yaricap = double.Parse(Console.ReadLine());
+
+            alan = Math.PI * yaricap * yaricap;
+            Console.WriteLine("dairenin alani: " + alan);
+            Console.ReadLine();
+        }
+    }
+}
