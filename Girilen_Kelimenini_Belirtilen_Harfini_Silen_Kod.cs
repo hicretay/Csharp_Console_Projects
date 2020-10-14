ddbe62bf2a72bdb78091ef2c0b9db3d3e75e5a4e
Hicret Ay <46728765+hicrett@users.noreply.github.com>
2020-10-14 11:25:41 +0300

    14.10.2020

---
+++ Girilen_Kelimenini_Belirtilen_Harfini_Silen_Kod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace example_2
+{
+    class Program
+    {
+        static void Harf_Sil()
+        {
+
+            Console.WriteLine("Bir kelime giriniz: ");
+            string kelime = Console.ReadLine();
+
+            Console.WriteLine("Kaçıncı karakter kaldırılsın: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            if ((kelime.Length >= sayi)|| (sayi < 0))
+            {
+                string rem = kelime.Remove(sayi - 1, 1);
+                Console.WriteLine(rem);
+            }
+            else
+            {
+
+                Console.WriteLine("Hatalı giriş !");
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Harf_Sil();
+            Console.ReadLine();
+        }
+        
+    }
+}
